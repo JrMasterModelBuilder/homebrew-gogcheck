751d8b671d93250432ae0c0be2ade7355b716cc4
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2024-01-29 19:04:52 -0500

    Added GOGCHECK_CERTFILE env var

--- Formula/gogcheck.rb
+++ Formula/gogcheck.rb
@@ -3,6 +3,7 @@ class Gogcheck < Formula
   homepage "https://github.com/hippie68/gogcheck"
   url "https://github.com/JrMasterModelBuilder/homebrew-gogcheck/releases/download/sources/gogcheck-2023-09-28-ad16558a86af4cbd28d6b028464d956c4e567db8.zip"
   version "2023-09-28"
+  revision 1
   sha256 "42e7248d010a2ce4683669822911f835f3e3e85a35e1de08b92d9a7dcae45d3e"
 
   on_macos do
@@ -14,6 +15,7 @@ class Gogcheck < Formula
   def install
     inreplace "gogcheck" do |s|
       s.gsub! "#!/bin/bash", "#!/usr/bin/env bash"
+      s.gsub! "#certfile=", 'certfile="${GOGCHECK_CERTFILE:-}"'
       if OS.mac?
         s.gsub! /\bgrep\b/, "ggrep"
         s.gsub! /\bfind\b/, "gfind"
